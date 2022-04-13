afbe1d21b0e8122e309982c49f60e2394e756507
José Miguel Chávez <josemchavezg@Joses-Mac-mini.local>
2022-04-13 19:48:16 -0400

    primer commit

---
+++ src/main/java/com/app/app_eval_1/AppEval1Application.java
@@ -0,0 +1,13 @@
+package com.app.app_eval_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEval1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEval1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/app_eval_1/AppEval1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.app_eval_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEval1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
